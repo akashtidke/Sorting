ae27991bbe356cc83533d33e534853c12e6348da
akash <akashtidke2014@gmail.com>
2021-11-13 20:24:03 +0530

    updated

--- mergeSort.java
+++ mergeSort.java
@@ -1,10 +1,57 @@
+import java.util.Arrays;
+
 public class mergeSort {
     public static void main(String[] args) {
-        int []arr1={1,5,7,3,8,10,2};
-        merge(arr1);
+        int []arr1={5,7,3,8,10,2};
+        System.out.println(Arrays.toString(merge(arr1)));
+        System.out.println(Arrays.toString(arr1));
+
     }
-    static void merge(int []arr){
+    static int [] merge(int []arr){
+       if (arr.length==1){
+           return arr ;
+       }
+        int mid=arr.length/2;
+        int[] start=merge(Arrays.copyOfRange(arr,0,mid));
+        int []end=merge(Arrays.copyOfRange(arr,mid,arr.length));
+
+        return sort2Arrays(start,end);
+
 
         }
+        static int[]sort2Arrays(int []start,int[]end){
+        int[] ans=new int[start.length+end.length];
+
+        int i=0;
+        int j=0;
+        int k=0;
+        while (i<start.length&&j<end.length) {
+            if (start[i] > end[j]) {
+                ans[k] = end[j];
+                k++;
+                j++;
+            } else  {
+                ans[k] = start[i];
+                i++;
+                k++;
+            }
+        }
+        while (i<start.length){
+            ans[k]=start[i];
+            i++;
+            k++;
+        }
+            while (j<end.length){
+                ans[k]=end[j];
+                j++;
+                k++;
+            }
+
+        return ans;
+        }
+
+
+
+
 }
 
--- t3sumtarget.java
+++ t3sumtarget.java
@@ -2,24 +2,26 @@ import java.util.Arrays;
 
 public class t3sumtarget {
     public static void main(String[] args) {
-        int[]arr={3,6,2,5,7,4};
-        int target=10;
+        int[]arr={3,6,2,5,7,4,8,9,7,18};
+        int target=22;
         triplet(arr,target);
     }
     static void triplet(int []arr,int target){
         Arrays.sort(arr);
         //System.out.println(Arrays.toString(arr));
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length-1; i++) {
             int newtarget=target-arr[i];
             int left=i+1;
             int right=arr.length-1;
-            while (right<left){
+            while (left<right){
                 if (arr[left]+arr[right]>newtarget){
-                    left--;
-                } else if (arr[left]+arr[right]<newtarget) {
-                    right++;
+                    right--;
+                }else if (arr[left]+arr[right]<newtarget){
+                    left++;
                 }else {
                     System.out.println(arr[i]+" "+arr[left]+" "+arr[right]);
+                    left++;
+                    right--;
                 }
             }
         }
